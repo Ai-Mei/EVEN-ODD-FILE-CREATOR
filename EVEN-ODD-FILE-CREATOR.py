8a47ecc257a0c4e15ee0ebdfcd88ea0e59a2cb0b
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-22 15:31:01 +0800

    Generated 20 random integers and saved it to numbers.txt.

--- EVEN-ODD-FILE-CREATOR.py
+++ EVEN-ODD-FILE-CREATOR.py
@@ -1,1 +1,16 @@
+# # Open the numbers.txt.
+with open("numbers.txt", "a") as numbers_file:
+# numbers_file = open("numbers.txt", "a")
 
+
+
+# Generate 20 random integers.
+    import random
+    i = 0
+    for i in range (20):
+        integer = random.randint(1, 100)
+        numbers_file.write(str(integer) + "\n")
+        print(integer)
+        i += 1
+
+numbers_file.close()
